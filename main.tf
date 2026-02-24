ef0e3b0033c74e83f3ccb33dfba9611b8db48aff
Manojna Mitali <mitalimanojna@gmail.com>
2026-02-24 14:54:04 +0530

    Update AMI, key name, and security group for instance

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-0ecb62995f68bb549"
+  ami                    = "ami-0b6c6ebed2801a5cb"
   instance_type          = "c7i-flex.large"
-  key_name               = "rahamdocker"
-  vpc_security_group_ids = ["sg-046cd16cdb7a78595"]
+  key_name               = "majorproject"
+  vpc_security_group_ids = ["sg-04a0ee6c90761cb68"]
   tags = {
     Name = var.instance_names[count.index]
   }
