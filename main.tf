745ab52b10016442a993fa65c48e787dbe362d9f
Manojna Mitali <mitalimanojna@gmail.com>
2026-02-25 11:51:44 +0530

    Update security group ID in main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "one" {
   ami                    = "ami-0b6c6ebed2801a5cb"
   instance_type          = "c7i-flex.large"
   key_name               = "majorproject"
-  vpc_security_group_ids = ["sg-04a0ee6c90761cb68"]
+  vpc_security_group_ids = ["sg-0f84108a9de2033bf"]
   tags = {
     Name = var.instance_names[count.index]
   }
